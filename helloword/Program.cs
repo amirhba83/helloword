697a462a46c358d410463dffc35a04246844c49e
amirh <amirh@DESKTOP-M08CGOK>
2024-08-09 11:48:59 +0330

    Add project files.

---
+++ helloword/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace helloword
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine(1+1);
+
+            }
+            
+
+        }
+    }
+}
